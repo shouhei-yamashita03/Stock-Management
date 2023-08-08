d88f703ccc1b4ac553e76570f53b881817c1e091
shouhei-yamashita03 <xiangpingshanxia757@gmail.com>
2023-08-08 15:58:23 +0900

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
-end
+
+    def full_title(page_name = "")
+      base_title = "Stock-ManegemntApp"
+      if page_name.empty?
+        base_title
+      else
+        page_name + " | " + base_title
+      end
+    end
+  end
