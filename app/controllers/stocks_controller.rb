8bc533bcd5e476c4fbd2883f1302014a6e651c4e
shouhei-yamashita03 <xiangpingshanxia757@gmail.com>
2023-10-06 00:26:36 +0900

    Finish warehouse-arrangement-page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -47,26 +47,22 @@ class StocksController < ApplicationController
 
   # 在庫検索
   def stock_search
-    #@user = User.find(params[:user_id])
     @user = current_user
-    # params[:q]はこの後に作成するビューファイルから送られてくるパラメーター
+    # params[:q]はビューファイルから送られてくるパラメーター
     # ransackメソッドは送られてきたパラメーターを元にテーブルからデータを検索するメソッド
     # resultメソッドはransackメソッドで取得したデータをActiveRecord_Relationのオブジェクトに変換するメソッド
+    # distinct: trueは重複したデータを除外
     @q = Stock.ransack(params[:q])
-    @results = @q.result
-    @result = params[:q]&.values&.reject(&:blank?)
+    # @results = @q.result(distinct: true)
+    @results = @q.result(distinct: true)
+    # @result = params[:q]&.values&.reject(&:blank?)
   end
 
   # 検索結果
   def search_result
     @user = current_user
-    @stocks = @user.stocks
-  end
-
-  # 在庫詳細
-  def stock_detail
-    @user = current_user
-    @stocks = @user.stocks
+    @q = Stock.ransack(params[:q])
+    @results = @q.result(distinct: true)
   end
 
   # 検索結果編集
@@ -87,30 +83,11 @@ class StocksController < ApplicationController
     redirect_to search_result_user_stock_url
   end
 
-  # 在庫詳細編集
-  def edit_stock_detail
-    @user = current_user
-    @stock = @user.stocks.find(params[:user_id])
-    @user_stocks = @user.stocks
-  end
-
-  def update_stock_detail
-    @user = current_user
-    @stock = @user.stocks.find(params[:id])
-    if @stock.update_attributes(stock_detail_params)
-      flash[:success] = "更新しました。"
-    else
-      flash[:danger] = "更新は失敗しました。"
-    end
-    redirect_to stock_detail_user_stock_url
-
-  end
-
-  def destroy_stock_detail
+  def destroy_search_result
     @user = current_user
     @stock = @user.stocks.find(params[:user_id])
     @stock.destroy
-    flash[:success] = "在庫詳細情報を削除しました。"
+    flash[:success] = "検索結果を削除しました。"
     redirect_to search_result_user_stock_url(@stock)
   end
 
@@ -128,9 +105,5 @@ class StocksController < ApplicationController
 
   def search_result_params
     params.require(:stock).permit(:display_number, :category_name, :stock_name, :stock_description, :price, :quantity)
-  end
-
-  def stock_detail_params
-    params.require(:stock).permit(:category_name, :stock_name, :stock_description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,7 @@ Rails.application.routes.draw do
       get 'stock_detail'
       get 'edit_search_result'
       patch 'update_search_result'
-      get 'edit_stock_detail'
-      patch 'update_stock_detail'
-      delete 'stock_details/:id', to: 'stocks#destroy_stock_detail', as: 'destroy_stock_detail'
+      delete 'search_result/:id', to: 'stocks#destroy_search_result', as: 'destroy_search_result'
       get 'warehouse_arrangement'
     end
     end
